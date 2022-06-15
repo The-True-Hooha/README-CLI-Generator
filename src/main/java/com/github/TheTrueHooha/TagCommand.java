7df5915776d0c74d7fea79f157492797a9aee74c
David Ogar <owogogahhero@outlook.com>
2022-06-15 14:28:56 +0100

    feat: added tag commmands

---
+++ src/main/java/com/github/TheTrueHooha/TagCommand.java
@@ -0,0 +1,14 @@
+package com.github.TheTrueHooha;
+
+import picocli.CommandLine.*;
+
+@Command(name = "tag",
+        description = "tag your preferred framework",
+        mixinStandardHelpOptions = true)
+public class TagCommand implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
